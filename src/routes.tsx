ce9e2dd11518adb0177cbdbe425229faf38fd19b
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-10-16 22:19:12 -0300

    Create routes

---
+++ src/routes.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import OrphanagesMap from './pages/OrphanagesMap';
+import OrphanageDetails from './pages/OrphanageDetails';
+
+const { Navigator, Screen } = createStackNavigator();
+
+export default () => {
+  return (
+    <NavigationContainer>
+      <Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Screen name="OrphanageMap" component={OrphanagesMap} />
+        <Screen name="OrphanageDetails" component={OrphanageDetails} />
+      </Navigator>
+    </NavigationContainer>
+  );
+};
