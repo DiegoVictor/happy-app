c0edabbf064d1889d0d568d5551bc5c8574d4243
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-10-17 22:51:44 -0300

    Update details page

--- src/pages/OrphanageDetails/index.tsx
+++ src/pages/OrphanageDetails/index.tsx
@@ -1,10 +1,109 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView, Text, StyleSheet } from 'react-native';
+import { Marker } from 'react-native-maps';
+import { Feather, FontAwesome } from '@expo/vector-icons';
 
-// import { Container } from './styles';
+import mapMarkerImg from '../../images/map-marker.png';
+import {
+  Container,
+  ImagesContainer,
+  Img,
+  DetailsContainer,
+  Title,
+  Description,
+  Separator,
+  ScheduleContainer,
+  ContactButton,
+  ContactButtonText,
+  MapContainer,
+  Map,
+  RoutesContainer,
+  RoutesText,
+  ScheduleItem,
+  ScheduleText,
+} from './styles';
 
-const OrphanageDetails: React.FC = () => {
-  return <View />;
-};
+export default function OrphanageDetails() {
+  return (
+    <Container>
+      <ImagesContainer>
+        <ScrollView horizontal pagingEnabled>
+          <Img
+            source={{
+              uri: 'https://fmnova.com.br/images/noticias/safe_image.jpg',
+            }}
+          />
+          <Img
+            source={{
+              uri: 'https://fmnova.com.br/images/noticias/safe_image.jpg',
+            }}
+          />
+          <Img
+            source={{
+              uri: 'https://fmnova.com.br/images/noticias/safe_image.jpg',
+            }}
+          />
+        </ScrollView>
+      </ImagesContainer>
 
-export default OrphanageDetails;
+      <DetailsContainer>
+        <Title>Orf. Esperança</Title>
+        <Description>
+          Presta assistência a crianças de 06 a 15 anos que se encontre em
+          situação de risco e/ou vulnerabilidade social.
+        </Description>
+
+        <MapContainer>
+          <Map
+            initialRegion={{
+              latitude: -27.2092052,
+              longitude: -49.6401092,
+              latitudeDelta: 0.008,
+              longitudeDelta: 0.008,
+            }}
+            zoomEnabled={false}
+            pitchEnabled={false}
+            scrollEnabled={false}
+            rotateEnabled={false}
+          >
+            <Marker
+              icon={mapMarkerImg}
+              coordinate={{
+                latitude: -27.2092052,
+                longitude: -49.6401092,
+              }}
+            />
+          </Map>
+
+          <RoutesContainer>
+            <RoutesText>Ver rotas no Google Maps</RoutesText>
+          </RoutesContainer>
+        </MapContainer>
+
+        <Separator />
+
+        <Title>Instruções para visita</Title>
+        <Description>
+          Venha como se sentir a vontade e traga muito amor e paciência para
+          dar.
+        </Description>
+
+        <ScheduleContainer>
+          <ScheduleItem color="blue">
+            <Feather name="clock" size={40} color="#2AB5D1" />
+            <ScheduleText color="blue">Segunda à Sexta 8h às 18h</ScheduleText>
+          </ScheduleItem>
+          <ScheduleItem color="green">
+            <Feather name="info" size={40} color="#39CC83" />
+            <ScheduleText color="green">Atendemos fim de semana</ScheduleText>
+          </ScheduleItem>
+        </ScheduleContainer>
+
+        <ContactButton onPress={() => {}}>
+          <FontAwesome name="whatsapp" size={24} color="#FFF" />
+          <ContactButtonText>Entrar em contato</ContactButtonText>
+        </ContactButton>
+      </DetailsContainer>
+    </Container>
+  );
+}
---
+++ src/pages/OrphanageDetails/styles.ts
@@ -0,0 +1,141 @@
+import { Dimensions } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import MapView from 'react-native-maps';
+import styled, { css } from 'styled-components/native';
+
+interface ScheduleColors {
+  color: 'blue' | 'green';
+}
+
+export const Container = styled.ScrollView`
+  flex: 1;
+`;
+
+export const ImagesContainer = styled.View`
+  height: 240px;
+`;
+
+export const Img = styled.Image.attrs({
+  resizeMode: 'cover',
+})`
+  height: 240px;
+  width: ${Dimensions.get('window').width}px;
+`;
+
+export const DetailsContainer = styled.View`
+  padding: 24px;
+`;
+
+export const Title = styled.Text`
+  color: #4d6f80;
+  font-family: Nunito_700Bold;
+  font-size: 30px;
+`;
+
+export const Description = styled.Text`
+  color: #5c8599;
+  font-family: Nunito_600SemiBold;
+  line-height: 24px;
+  margin-top: 16px;
+`;
+
+export const ScheduleContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 24px;
+`;
+
+export const ContactButton = styled(RectButton)`
+  align-items: center;
+  background-color: #3cdc8c;
+  border-radius: 20px;
+  flex-direction: row;
+  height: 56px;
+  justify-content: center;
+  margin-top: 40px;
+`;
+
+export const ContactButtonText = styled.Text`
+  color: #fff;
+  font-family: Nunito_800ExtraBold;
+  font-size: 16px;
+  margin-left: 16px;
+`;
+
+export const Separator = styled.View`
+  background-color: #d3e2e6;
+  height: 0.8px;
+  margin: 40px 0px;
+  width: 100%;
+`;
+
+export const RoutesContainer = styled.View`
+  align-items: center;
+  justify-content: center;
+  padding: 16px;
+`;
+
+export const RoutesText = styled.Text`
+  color: #0089a5;
+  font-family: Nunito_700Bold;
+`;
+
+export const MapContainer = styled.View`
+  background-color: #e6f7fb;
+  border-color: #b3dae2;
+  border-radius: 20px;
+  border-width: 1.2px;
+  overflow: hidden;
+  margin-top: 40px;
+`;
+
+export const Map = styled(MapView)`
+  height: 150px;
+  width: 100%;
+`;
+
+export const ScheduleItem = styled.View<ScheduleColors>`
+  padding: 20px;
+  width: 48%;
+
+  ${props => {
+    switch (props.color) {
+      case 'blue':
+        return css`
+          background-color: #e6f7fb;
+          border-width: 1px;
+          border-color: #b3dae2;
+          border-radius: 20px;
+        `;
+
+      case 'green':
+        return css`
+          background-color: #edfff6;
+          border-color: #a1e9c5;
+          border-radius: 20px;
+          border-width: 1px;
+        `;
+    }
+  }}
+`;
+
+export const ScheduleText = styled.Text<ScheduleColors>`
+  font-family: Nunito_600SemiBold;
+  font-size: 16px;
+  line-height: 24px;
+  margin-top: 20px;
+
+  ${props => {
+    switch (props.color) {
+      case 'blue':
+        return css`
+          color: #5c8599;
+        `;
+
+      case 'green':
+        return css`
+          color: #37c77f;
+        `;
+    }
+  }}
+`;
