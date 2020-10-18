88cb107787f0c111cbfd610ffb2ad0843dc357b3
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-10-17 21:43:01 -0300

    Create nwe routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import OrphanagesMap from '../pages/OrphanagesMap';
 import OrphanageDetails from '../pages/OrphanageDetails';
+import SelectMapPosition from '../pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from '../pages/CreateOrphanage/OrphanageData';
+import Header from '../components/Header';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -13,10 +16,36 @@ const Routes: React.FC = () => {
       <Navigator
         screenOptions={{
           headerShown: false,
+          cardStyle: {
+            backgroundColor: '#f2f3f5',
+          },
         }}
       >
         <Screen name="OrphanageMap" component={OrphanagesMap} />
-        <Screen name="OrphanageDetails" component={OrphanageDetails} />
+        <Screen
+          name="OrphanageDetails"
+          component={OrphanageDetails}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Orfanato" showCancel={false} />,
+          }}
+        />
+        <Screen
+          name="SelectMapPosition"
+          component={SelectMapPosition}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Selecione no mapa" />,
+          }}
+        />
+        <Screen
+          name="OrphanageData"
+          component={OrphanageData}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Preencha os campos" />,
+          }}
+        />
       </Navigator>
     </NavigationContainer>
   );
