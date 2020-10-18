8a953f0731e85ea7130a29059cb67c4f7413d8d9
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-10-17 22:15:10 -0300

    Create input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useRef } from 'react';
+import { TextInputProps } from 'react-native';
+import { useField } from '@unform/core';
+
+import { Container } from './styles';
+
+interface InputProps extends TextInputProps {
+  name: string;
+}
+
+interface InputRef {
+  focus(): void;
+}
+
+const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = ({
+  name,
+  ...rest
+}) => {
+  const inputRef = useRef<any>(null);
+  const { fieldName, registerField, defaultValue, error } = useField(name);
+
+  useEffect(() => {
+    registerField<string>({
+      name: fieldName,
+      ref: inputRef.current,
+      clearValue(ref) {
+        ref.value = '';
+
+        ref.clear();
+      },
+      setValue(ref: any, value) {
+        ref.setNativeProps({ text: value });
+        inputRef.current.value = value;
+      },
+      getValue(ref) {
+        return ref.value;
+      },
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container
+      ref={inputRef}
+      keyboardAppearance="dark"
+      defaultValue={defaultValue}
+      onChangeText={value => {
+        if (inputRef.current) {
+          inputRef.current.value = value;
+        }
+      }}
+      {...rest}
+    />
+  );
+};
+
+export default Input;
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.TextInput.attrs({
+  textAlignVertical: 'top',
+})`
+  background-color: #fff;
+  border-width: 1.4px;
+  border-color: #d3e2e6;
+  border-radius: 20px;
+  height: 56px;
+  padding: 18px 24px;
+  margin-bottom: 16px;
+`;
