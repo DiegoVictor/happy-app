3ba40730b027557da590c97ae565536075b9a1ad
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-10-26 18:03:50 -0300

    Create custom test changes listener

---
+++ tests/utils/wait.ts
@@ -0,0 +1,30 @@
+import { act } from '@testing-library/react-native';
+
+async function wait<T>(expectation: () => T): Promise<T> {
+  const startTime = Date.now();
+
+  return await act(
+    async (): Promise<T> => {
+      return new Promise((resolve, reject) => {
+        const rejectOrRerun = (error: Error) => {
+          if (Date.now() - startTime >= 4500) {
+            reject(error);
+            return;
+          }
+          setTimeout(runExpectation, 50);
+        };
+        function runExpectation() {
+          try {
+            const result = expectation();
+            resolve(result);
+          } catch (error) {
+            rejectOrRerun(error);
+          }
+        }
+        setTimeout(runExpectation, 0);
+      });
+    },
+  );
+}
+
+export default wait;
