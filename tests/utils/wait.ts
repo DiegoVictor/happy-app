fe573b65fcd6c0c0c42dadd8ef8c6cffea684e4d
Diego Victor <diegovictorgonzaga@gmail.com>
2022-06-09 18:36:11 -0300

    Fix from lint

--- tests/utils/wait.ts
+++ tests/utils/wait.ts
@@ -3,28 +3,26 @@ import { act } from '@testing-library/react-native';
 async function wait<T>(expectation: () => T): Promise<T> {
   const startTime = Date.now();
 
-  return await act(
-    async (): Promise<T> => {
-      return new Promise((resolve, reject) => {
-        const rejectOrRerun = (error: Error) => {
-          if (Date.now() - startTime >= 4500) {
-            reject(error);
-            return;
-          }
-          setTimeout(runExpectation, 50);
-        };
-        function runExpectation() {
-          try {
-            const result = expectation();
-            resolve(result);
-          } catch (error) {
-            rejectOrRerun(error);
-          }
+  return await act(async (): Promise<T> => {
+    return new Promise((resolve, reject) => {
+      const rejectOrRerun = (error: Error) => {
+        if (Date.now() - startTime >= 4500) {
+          reject(error);
+          return;
         }
-        setTimeout(runExpectation, 0);
-      });
-    },
-  );
+        setTimeout(runExpectation, 50);
+      };
+      function runExpectation() {
+        try {
+          const result = expectation();
+          resolve(result);
+        } catch (error) {
+          rejectOrRerun(error as Error);
+        }
+      }
+      setTimeout(runExpectation, 0);
+    });
+  });
 }
 
 export default wait;
